5f9462aea836063629397f17c6df7e183b69ac91
amine-zzr <aminezoukri@gmail.com>
2024-05-18 23:05:05 +0000

    handling default and dynamic text input

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''starting a web app'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''Display "Hello HBNB!" on the root route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Display "HBNB" on the hbnb route'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    ''' display “C” followed by the value of the text variable'''
+    new_text = text.replace('_', ' ')
+    return f'C {new_text}'
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    ''' display “Python” followed by the value of the text variable'''
+    new_text = text.replace('_', ' ')
+    return f'Python {new_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
