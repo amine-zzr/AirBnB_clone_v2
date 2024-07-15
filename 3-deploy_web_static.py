18d00db816679328b4027a5cd78e926ed83ce85d
amine-zzr <aminezoukri@gmail.com>
2024-07-15 00:47:09 +0000

    simple fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Orchestrates the archive creation and deployment process.'''
+'''Orchestrates the archive creation and deployment process. '''
 
 from fabric.api import env, put, run, local
 import os
